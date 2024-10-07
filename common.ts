90fdd9481a4c2f42a25ed14e2b722b4673d031f3
吴铭 <451517996@qq.com>
2024-10-07 22:10:04 +0800

    更新技能枚举

--- common.ts
+++ common.ts
@@ -79,7 +79,8 @@ export enum Direction{
     Left = 4,
     Self = 5,
     Cross = 6,
-    Summon = 7
+    Summon = 7,
+    Trigger = 8
 }
 
 export enum BufferAndEquipEffect{
@@ -125,7 +126,8 @@ export enum EMSkillEvent{
     self_front_null = 28,
     enemy_front_null = 29,
     start_round_vacancy = 30,
-    start_round_victory = 31
+    start_round_victory = 31,
+    self_upgrade = 32
 }
 
 export enum SkillEffectEM{
@@ -156,7 +158,8 @@ export enum SkillEffectEM{
     AddPropertyShopLeft = 25,
     BankCoin = 26,
     AddPropertyLevel3 = 27,
-    CopySkill = 28
+    CopySkill = 28,
+    NextRoundCoin = 29
 }
 
 export enum ShopIndex{
