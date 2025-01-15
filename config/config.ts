1ca5f0d4d65e9f4a08db5f97374c93a64fa14cb2
吴铭 <451517996@qq.com>
2025-01-15 23:20:20 +0800

    整理预加载代码

--- config/config.ts
+++ config/config.ts
@@ -6,7 +6,6 @@
 import * as skill_config from './skill_config'
 import * as buffer_config from './buffer_config'
 import * as role_config from './role_config'
-import * as bundle_config from './bundle_config'
 import * as Food_config from './Food_config';
 import * as fetters from './fetters_config'
 import * as Equip_Config from './Equip_config';
@@ -27,7 +26,6 @@ export class config {
     public static FettersConfig:Map<number,fetters.FettersConfig>
     public static BufferConfig : Map<number, buffer_config.BufferConfig>;
     public static RoleConfig : Map<number, role_config.RoleConfig>;
-    public static BundleConfig:Map<number, bundle_config.BundleConfig>;
     public static FoodConfig:Map<number,Food_config.FoodConfig>;
     public static EquipConfig:Map<number,Equip_Config.EquipConfig>;
     public static TaskConfig:Map<number,task_config.TaskConfig>;
@@ -46,7 +44,6 @@ export class config {
         config.FettersConfig = await fetters.LoadFettersConfig();
         config.BufferConfig = await buffer_config.LoadBufferConfig();
         config.RoleConfig = await role_config.LoadRoleConfig();
-        config.BundleConfig = await bundle_config.LoadBundleConfig();
         config.FoodConfig = await Food_config.LoadFoodConfig();
         config.EquipConfig = await Equip_Config.LoadEquipConfig();
         config.TaskConfig = await task_config.LoadTaskConfig();
